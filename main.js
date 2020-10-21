a6c31c1b3a14cd44fa4774b2fe1f77e66361f985
jayeewong <962326693@qq.com>
2020-10-21 16:45:10 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ auto();
 console.show();
 var postpone = rawInput("全局延迟设定(建议1-5，越低越快，越高越稳定但慢");
 postpone = postpone * 1000;
-//launch("com.taobao.taobao");
 
 console.log("准备进入活动界面，若未出现进入界面成功的提示，请开高延迟");
 
@@ -13,76 +12,14 @@ app.startActivity({
 })
 
 sleep(5000+postpone);
-/*
-console.log("准备进入活动界面，若未出现进入界面成功的提示，请开高延迟");
-desc("搜索").findOne().click();
-sleep(postpone);
-id("searchEdit").findOne().setText("双十一");
-sleep(postpone);
-id("searchbtn").findOne().click();
-sleep(postpone * 2);
 
-var enter = desc("进入").findOne();
-click(enter.bounds().centerX(),
-    enter.bounds().centerY());
-sleep(postpone * 2);
-text("活动链接").findOne().click();
-sleep(postpone * 2);
-*/
 console.log("进入活动界面成功");
 
 text("赚喵币").findOne().click();
 sleep(postpone * 2);
 console.log("开始执行任务");
 
-/*
-while (true) {
-    var t=new Date();
-    var golook = text("去浏览").findOnce();
-    if (golook) {
-        golook.click();
-        var x=random(1,5);
-        while(x--){
-        sleep(5000+t.getSeconds()*10);
-        swipe(700+t.getSeconds(),
-        2000-t.getSeconds()*10,
-        200+t.getMilliseconds(),
-        700+t.getMilliseconds(),
-        1000);
-        }
-        sleep(15000+t.getSeconds()*10);
-        back();
-        sleep(1000);
-    } else
-        break;
-}
 
-while (true) {
-    var t=new Date();
-    var golook = text("去完成").findOnce(1);
-    if (golook) {
-        golook.click();
-        var x=random(1,5);
-        while(x--){
-        sleep(5000+t.getSeconds()*10);
-        swipe(700+t.getSeconds(),
-        2000-t.getSeconds()*10,
-        200+t.getMilliseconds(),
-        700+t.getMilliseconds(),
-        1000);
-        }
-        if (text("50000").findOnce()) {
-            back();
-            continue;
-        }
-        sleep(15000+t.getSeconds()*10);
-        back();
-        sleep(1000);
-    } else
-        break;
-}
-
-*/
 function mission(s) {
     var start = 0;
     if (s == "去完成") start = 1;
@@ -119,6 +56,8 @@ mission("去完成");
 console.log("看看完成");
 mission("去逛逛");
 console.log("逛逛完成");
+mission("去搜索");
+console.log("搜索完成");
 
 
 console.log("浏览任务已经完成");
