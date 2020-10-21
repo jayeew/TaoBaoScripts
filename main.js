823ebaccc921565e4128f7df0c97d30b4af4decf
jayeewong <962326693@qq.com>
2020-10-21 09:45:34 +0800

    增加随机滑动，随机位置点猫

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 auto();
 console.show();
 var postpone = rawInput("全局延迟设定(建议1-5，越低越快，越高越稳定但慢");
-postpone=postpone*1000;
+postpone = postpone * 1000;
 launch("com.taobao.taobao");
 sleep(5000);
 /*
@@ -14,36 +14,57 @@ sleep(postpone);
 id("searchEdit").findOne().setText("双十一");
 sleep(postpone);
 id("searchbtn").findOne().click();
-sleep(postpone*2);
+sleep(postpone * 2);
 
-var enter=desc("进入").findOne();
+var enter = desc("进入").findOne();
 click(enter.bounds().centerX(),
-enter.bounds().centerY());
-sleep(postpone*2);
+    enter.bounds().centerY());
+sleep(postpone * 2);
 text("活动链接").findOne().click();
-sleep(postpone*2);
+sleep(postpone * 2);
 text("赚喵币").findOne().click();
-sleep(postpone*2);
+sleep(postpone * 2);
+
 while (true) {
+    var t=new Date();
     var golook = text("去浏览").findOnce();
     if (golook) {
         golook.click();
-        sleep(23000);
+        var x=random(1,5);
+        while(x--){
+        sleep(5000+t.getSeconds()*10);
+        swipe(700+t.getSeconds,
+        2000-t.getSeconds()*10,
+        200+t.getMilliseconds(),
+        700+t.getMilliseconds(),
+        1000);
+        }
+        sleep(15000+t.getSeconds()*10);
         back();
-        sleep(500);
+        sleep(1000);
     } else
         break;
 }
 
 while (true) {
+    var t=new Date();
     var golook = text("去完成").findOnce(1);
     if (golook) {
         golook.click();
-        sleep(10000);
-        if(text("50000").findOnce()){
+        var x=random(1,5);
+        while(x--){
+        sleep(5000+t.getSeconds()*10);
+        swipe(700+t.getSeconds,
+        2000-t.getSeconds()*10,
+        200+t.getMilliseconds(),
+        700+t.getMilliseconds(),
+        1000);
+        }
+        if (text("50000").findOnce()) {
             back();
-            continue;}
-        sleep(13000);
+            continue;
+        }
+        sleep(15000+t.getSeconds()*10);
         back();
         sleep(1000);
     } else
@@ -62,24 +83,24 @@ while (true) {
         receive.click();
         //sleep(postpone);
     }
-    var non_enough=text("哎哟，喵币不足啦").findOnce();
-    if(non_enough){
+    var non_enough = text("哎哟，喵币不足啦").findOnce();
+    if (non_enough) {
         indexInParent(3).text("关闭").findOne().click();
         break;
-        }
-    var decoration=text("领取成就勋章").findOnce();
-    if(decoration){
+    }
+    var decoration = text("领取成就勋章").findOnce();
+    if (decoration) {
         indexInParent(4).text("关闭").findOne().click();
         break;
-        }
+    }
 }
 console.log("执行完毕，开始拍猫");
 
-var num=rawInput("输入拍猫次数（中途不可退，次数别太多）");
-while(num--){
-    
-text("我的猫，点击撸猫").findOne().click();
-sleep(200);
+var num = rawInput("输入拍猫次数（中途不可退，次数别太多）");
+while (num--) {
+
+    var cat=text("我的猫，点击撸猫").findOne();
+    click(cat.bounds().centerX()+random(1,5)*5,
+    cat.bounds().centerY()+random(1,5)*5);
+    sleep(200);
 }
-
-
